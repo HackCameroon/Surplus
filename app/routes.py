11c0324d538f33eda357bd666421ec58e0ae3aa5
sallywit <c00163ans14@gmail.com>
2020-04-11 01:50:29 -0400

    geo_data hold user's current location

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+import requests
+
 
 
 @app.route('/')
@@ -22,6 +24,14 @@ def login():
 
 @app.route('/account')
 def account():
+
+	ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+	my_ip = ip_request.json()['ip'] 
+
+	geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
+	geo_request = requests.get(geo_request_url)
+	geo_data = geo_request.json()
+	print(geo_data)
 	user = {"username": "La Barca"}
 	items = [
 				{
@@ -41,4 +51,4 @@ def account():
 					'price': 0.00
 				} 
 			]
-	return render_template('account.html', title="Account", items=items, user=user)
+	return render_template('account.html', title="Account", items=items, user=user, geo_data=geo_data)
