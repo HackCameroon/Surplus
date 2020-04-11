36a687c3c9fcb1d5f9db0774ecad0943346aca3c
sallywit <c00163ans14@gmail.com>
2020-04-10 23:48:28 -0400

    login

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,6 @@ def index():
 	user = {"username": "Sally"}
 	return render_template('index.html', title="Home", user=user)
 
-@app.route('/login')
+@app.route(/'login')
 def login():
 	return render_template('account.html', title="Login")
