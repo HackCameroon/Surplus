b6071c1907b2541b83c7ea2d4bc37417e0ebdd5a
Anna Szymanski <acszyman@usc.edu>
2020-04-10 20:44:13 -0700

    add form html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route('/')
 @app.route('/index')
@@ -9,7 +11,11 @@ def index():
 	user = {"username": "Sally"}
 	return render_template('index.html', title="Home", user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect('/index')
 	return render_template('login.html', title='Sign In',form=form)
