9bca6d3bc1e380ff6f73c2734b522b6a71c1ced5
sallywit <c00163ans14@gmail.com>
2020-04-10 21:37:49 -0400

    account.html and route

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,7 @@ from app import app
 def index():
 	user = {"username": "Sally"}
 	return render_template('index.html', title="Home", user=user)
+
+@app.route('/login')
+def login():
+	return render_template('account.html', title="Login")
