eedad32c937142d2a3b5b763f1fc3b68f45cbb13
ehaaijer17 <haaijer@usc.edu>
2020-04-12 23:08:42 -0600

    this is my happy file

--- app/models.py
+++ app/models.py
@@ -1,29 +1,45 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-from app import db
+directoryPath = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(directoryPath, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-class Test(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    first = db.Column(db.String(64), index=True, unique=True)
-    last = db.Column(db.String(400), unique=True)
+db = SQLAlchemy(app)
 
-class Restaurant(db.Model):
-    restaurant_id = db. Column(db.Integer, primary_key = True)
-    r_name = db.Column(db.String(64), index=True, unique=True)
-    #r_address = db.Column(db.String(400), unique=True)
-    #phone = db.Column(db.String(20), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    #items = db.relationship('Item', backref='author', lazy='dynamic')
+class Seller(db.Model):
+    seller_id = db.Column(db.Integer, primary_key=True)
+    seller_email = db.Column(db.String, unique=True)
+    seller_name = db.Column(db.String)
+    seller_password = db.Column(db.String)
+    seller_zipcode = db.Column(db.Integer)
+    seller_phone = db.Column(db.Integer)
+    seller_address = db.Column(db.String)
+    def __init__(self, seller_id, seller_email, seller_name, seller_password, seller_zipcode, seller_phone, seller_address):
+        self.seller_id = seller_id
+        self.seller_email = seller_email
+        self.seller_name = seller_name
+        self.seller_password = seller_password
+        self.seller_zipcode = seller_zipcode
+        self.seller_phone = seller_phone
+        self.seller_address = seller_address
 
-    def __repr__(self):
-        return '<Restaurant {}>'.format(self.r_name)
-
-#class Item(db.Model):
-    #id = db.Column(db.Integer, primary_key = True)
-    #i_name = db.Column(db.String(100))
-    #i_description = db.Column(db.String(200))
-    #i_price = db.Column(db.Float(10))
-    #i_quantity = db.Column(db.Integer)
-    #rest_id = db.Column(db.Integer, db.ForeignKey('restaurant.id') )
+class Inventory(db.Model):
+    item_id = db.Column(db.Integer, primary_key=True)
+    seller_id = db.Column(db.Integer, foreign_key=True)
+    item_name = db.Column(db.String)
+    item_price = db.Column(db.String)
+    item_quantity = db.Column(db.Integer)
+    item_image = db.Column(db.String)
+    item_description = db.Column(db.String)
+    def __init__(self, item_id, seller_id, item_name, item_price, item_quantity, item_image, item_description):
+        self.item_id = item_id
+        self.seller_id = seller_id
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+        self.item_image = item_image
+        self.item_description = item_description
